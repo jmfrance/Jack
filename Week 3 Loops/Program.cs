a876a2ca78f277b71e4637d49fc2a48ae0e92b99
jmfrance <francemjack@gmail.com>
2023-10-13 14:49:46 +0100

    added code!!!!!!!!!!!

--- Week 3 Loops/Program.cs
+++ Week 3 Loops/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("1 Oldest Siblings");
+
+Console.WriteLine("Welcome to the Oldest Sibling Application.");
+
+Console.WriteLine("This application will tell us who the oldest of a pair of siblings is.");
+
+Console.WriteLine("First sibling, what is your name?");
+
+string name1 = Console.ReadLine();
+
+Console.WriteLine("Hello " + name1 + ", How old are you?");
+
+int age1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Second sibling, what is your name?");
+
+string name2 = Console.ReadLine();
+
+Console.WriteLine("Hello " + name2 + ", How old are you?");
+
+int age2 = int.Parse(Console.ReadLine());
+
+if (age1 > age2)
+{
+    Console.WriteLine(name1 + " is " + (age1 - age2) + " years older than " + name2);
+}
+
+if (age1 == age2)
+{
+    Console.WriteLine(name1 + " and " + name2 + " are the same age");
+}
