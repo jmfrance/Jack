8814e35342acfa4f03ad2ceeff1bd231b2c81611
jmfrance <146449780+jmfrance@users.noreply.github.com>
2023-10-27 19:48:14 +0100

    Update Program.cs

--- Week 3 Loops/Program.cs
+++ Week 3 Loops/Program.cs
@@ -1,44 +1,91 @@
-﻿Console.WriteLine("1 Oldest Siblings");
+﻿using System.ComponentModel.Design;
+using System.Reflection.Emit;
 
-Console.WriteLine("Welcome to the Oldest Sibling Application.");
+Console.WriteLine("Programming Portfolio results calculator");
+double digTotal = 0;
+double digRounded = 0;
+int digPort = -1;
+if (digPort < 0)
+{ 
+    Console.WriteLine("What was the digital portfolio mark? (out of 35)"); ;
+    diglabel: 
+    digPort = int.Parse(Console.ReadLine());
+    if (digPort >= 0 && digPort <= 35)
+    {
+        double digPercent = ((2.857) * digPort);
+        digRounded = (double)Math.Round(digPercent, 2);
+        digTotal = digRounded * 0.5; 
+        Console.WriteLine($"{digRounded}%");
+    }
+    while (digPort < 0 || digPort > 35)
+    {
+        Console.WriteLine("Please enter a value between 0 and 35."); ;
+        goto diglabel;
 
-Console.WriteLine("This application will tell us who the oldest of a pair of siblings is.");
-
-Console.WriteLine("First sibling, what is your name?");
-
-string name1 = Console.ReadLine();
-
-Console.WriteLine("Hello " + name1 + ", How old are you?");
-
-int age1 = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Second sibling, what is your name?");
-
-string name2 = Console.ReadLine();
-
-Console.WriteLine("Hello " + name2 + ", How old are you?");
-
-int age2 = int.Parse(Console.ReadLine());
-
-if (age1 > age2)
-{
-    Console.WriteLine(name1 + " is " + (age1 - age2) + " years older than " + name2);
+    }
 }
 
-if (age1 == age2)
+double openTotal = 0;
+double openRounded = 0;
+int openBook = -1;
+if (openBook < 0)
 {
-    Console.WriteLine(name1 + " and " + name2 + " are the same age");
+    Console.WriteLine("What was the open book mark? (out of 7)"); ;
+    openlabel:
+    openBook = int.Parse(Console.ReadLine());
+    if (openBook >= 0 && openBook <= 7)
+    {
+        double openPercent = ((14.286) * openBook);
+        openRounded = (double)Math.Round(openPercent, 2);
+        openTotal = openRounded * 0.25;
+        Console.WriteLine($"{openRounded}%");
+    }
+    while (openBook < 0 || openBook > 7)
+    {
+        Console.WriteLine("Please enter a value between 0 and 7.");
+        goto openlabel;
+    }
 }
 
-if (age1 > age2)
+double capTotal = 0;
+double capRounded = 0;
+int capstone = -1;
+if (capstone < 0)
 {
-    Console.WriteLine(name1 + " is " + (age1 - age2) + " years older than " + name2);
+    Console.WriteLine("What was the capstone project mark? (out of 100)");
+    caplabel:
+    capstone = int.Parse(Console.ReadLine());
+    if (capstone >= 0 && capstone <= 100)
+    {
+        double capPercent = ((1) * capstone);
+        capRounded = (double)Math.Round(capPercent, 2);
+        capTotal = capRounded * 0.25;
+        Console.WriteLine($"{capRounded}%");
+    }
+    while (capstone < 0 || capstone > 100)
+    {
+        Console.WriteLine("Please enter a value between 0 and 100."); ;
+        goto caplabel;
+    }
 }
-else if (age1 < age2)
+
+double total = capTotal + openTotal + digTotal;
+if (capTotal >= 40 && openTotal >= 40)
 {
-    Console.WriteLine(name2 + " is " + (age2 - age1) + " years older than " + name1);
-}
-else
-{
-    Console.WriteLine(name1 + " and " + name2 + " are the same age");
+    if (total >= 70)
+    {
+        Console.WriteLine($"Congratulations, you got a 1st with a percentage of {total}");
+    }
+    else if (total >= 60 && total < 70)
+    {
+        Console.WriteLine($"Congratulations, you got a 2:1 with a percentage of {total}");
+    }
+    else if (total >= 50 && total < 60)
+    {
+        Console.WriteLine($"Congratulations, you got a 2:2 with a percentage of {total}");
+    }
+    else if (total >= 40 && total < 50)
+    {
+        Console.WriteLine($"Congratulations, you got a 2:2 with a percentage of {total}");
+    }
 }
